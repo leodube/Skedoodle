9bb63e1b93941e9bcecfa1717160963493e114ce
Leo Dube <15lsd@caslab.queensu.ca>
2021-02-02 11:46:37 -0500

    Load svg title text on mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import { isMobile } from "react-device-detect";
 import { CodeLink, Idea, Definition } from "./components";
+import skedoodleImg from "./images/skedoodle.svg";
 
 const App = () => {
   return (
     <div className="App">
       <div className="container main-content">
         <div className="row justify-content-center">
-          <h1 id="main" className="text-center">
-            Skedoodle
-          </h1>
+          {isMobile ? (
+            <img src={skedoodleImg} />
+          ) : (
+            <h1 id="main" className="text-center">
+              Skedoodle
+            </h1>
+          )}
         </div>
         <div className="row justify-content-center">
           <Idea />
