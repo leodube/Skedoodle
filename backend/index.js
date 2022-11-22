ab9c7776ca9eeb809b1a26f70fb7530864d9bb86
Leo Dube <leo.s.dube@gmail.com>
2022-11-22 08:55:05 -0800

    Add rate limiting
    and move to yarn

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
-if (process.env.NODE_ENV !== 'production'){
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const DBNAME = process.env.DB_USER; 
+const DBNAME = process.env.DB_USER;
 const DBPASSWORD = process.env.DB_PASS;
 
-const uri =
-  `mongodb+srv://${DBNAME}:${DBPASSWORD}@cluster0.bauqt.mongodb.net/skedoodle?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${DBNAME}:${DBPASSWORD}@cluster0.bauqt.mongodb.net/skedoodle?retryWrites=true&w=majority`;
 
 // MODELS
 require("./models/Creature");
@@ -22,8 +22,17 @@ const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// MIDDLEWARE
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30, // Limit each IP to 30 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(limiter);
 
 // ROUTE
 require("./routes/creatureRoutes")(app);
